f7d04b1a738cafc8fe8db23da6b58a7ac1077814
kia0919 <john0256@naver.com>
2024-04-17 09:53:02 +0900

    2024.04.17 09:50

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import ZustandLibrary from 'src/zustand/ZustandLibrary';
 
 function App() {
   return (
-    <></>
+    <>
+      <ZustandLibrary />
+    </>
   );
 }
 
