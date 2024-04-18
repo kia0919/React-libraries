b9f7324d1dd378dbdff55421fe2a32b4385c3473
kia0919 <john0256@naver.com>
2024-04-18 11:51:50 +0900

    2024.04.18 11:50

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ZustandLibrary from 'src/zustand/ZustandLibrary';
 import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
 import Layout from './react-router/Layout';
+import AxiosLibrary from './axios/AxiosLibrary';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path='page2' element={<ReactRouterPage2 />} />
       </Route>
         <Route path='/zustand' element={<ZustandLibrary />} />
+        <Route path='/axios' element={<AxiosLibrary />} />
         {/* 올바르지 않은 주소 접속 시 404Error 표시 */}
         <Route path='*' element={<h3>404 Error</h3>} />
       </Routes>
--- src/axios/AxiosLibrary.tsx
+++ src/axios/AxiosLibrary.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios'
+import { error, time } from 'console';
 import React from 'react'
 
 //! axios 패키지(라이브러리) :
@@ -8,7 +10,39 @@ import React from 'react'
 
 //* - npm i axios
 export default function AxiosLibrary() {
-  return (
-    <div>AxiosLibrary</div>
-  )
+
+    const GET_URL = 'http://localhost:4100/api/v1/user/email@email.com';
+
+    //! axios 객체 :
+    // - axios 객체는 http method에 해당하는 함수들을 포함하고 있음
+    // - 각각의 http method 함수는 URL, option을 매개변수로 받음 
+    // - 각각의 http method 함수는 결과를 promise 형태로 반환
+    axios
+    .get(GET_URL)
+    //* 각각의 http method 함수는 결과는 then으로 받을땐 response객체를 
+    //* 매개변수로 받는 콜백함수를 전달
+    .then(response => {
+        //* response 객체는 status, header, data(body)를 포함하고 있음
+        //* then 작업은 status code가 400, 500번대가 아닌 response를 받아옴
+        console.log(response);
+    })
+    .catch(error => {
+        //* 해당 request에 대해서 request가 실패하거나 status code가 
+        //* 400, 500번대이면 catch로 AxiosErrod 객체를 받아옴
+        //* response가 존재하는 에러는 error객체에 response가 포함되어 있음
+        console.log(error);
+    })
+
+    //* option 매개변수
+    //* - request의 header를 조작할 때 사용됨
+    axios.get(GET_URL, {headers: { 'Authorization' : 'Bearer 12423145' },
+    params: {time: 1234} });
+
+    //! request body 매개변수 :
+    // - post, patch, put 함수는 두번째 매개변수로 request body 데이터를 전달함
+    const PATCH_URL = 'http://localhost:4100/api/v1/user/nickname'
+    axios.patch(PATCH_URL, { email: 'email@email.com', nickname: '코스피'  })
+
+    return <div>AxiosLibrary</div>
+
 }
